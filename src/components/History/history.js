80fdb9d988cc6f422a0f98f6aa7900e283f2330b
elliej1994 <102549808+elliej1994@users.noreply.github.com>
2022-05-11 20:13:45 -0400

    update history.js

--- src/components/History/history.js
+++ src/components/History/history.js
@@ -1,20 +1,23 @@
 import { useSelector } from 'react-redux';
 import './history';
+import '../Calculator/calculator.css';
 
 export default function History() {
     const historyList = useSelector(s => s.history);
 
     return (
         <>
+        <div className="historyContainer">
             <ul className="history">
                 {
                     historyList.map(h =>
-                        <li className='historyItem'>
+                        <li>
                             {h}
                         </li>
                     )
                 }
-            </ul>
+            </ul>     
+        </div>
         </>
     );
 }
